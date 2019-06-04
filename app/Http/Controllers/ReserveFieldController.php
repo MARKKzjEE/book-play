46cf1f90c7d1beaa5c50fbad8285ec45852e0ae7
Marc Gallego <marc.gallegog@e-campus.uab.cat>
2019-06-04 19:01:10 +0200

    Añadidos params y returns Controllers

--- app/Http/Controllers/ReserveFieldController.php
+++ app/Http/Controllers/ReserveFieldController.php
@@ -24,6 +24,8 @@ class ReserveFieldController extends Controller
       * establecimiento específico
      *
      * @author ArnauRovira
+      * @param int $id Número de id del establecimiento
+      * @return Establecimiento de la id específica
      */
     public function datosestablecimiento($id){
         return Establecimiento::datosestablecimiento($id);
@@ -37,6 +39,8 @@ class ReserveFieldController extends Controller
      * Recupera y retorna los datos de una
      * pista específico
      * @author ArnauRovira
+     * @param int $id Número de id de la pista
+     * @return Pista de la id específica
      */
     public function datospista($id){
         return Pista::datospista($id);
@@ -50,6 +54,13 @@ class ReserveFieldController extends Controller
      * los filtros de búsqueda que el usuario ha filtrado
      *
      * @author ArnauRovira
+     * @param string $superficie Superfície de la pista
+     * @param string $cercamiento Tipo de suelo de la pista
+     * @param string $pared Cercamiento de la pista
+     * @param string $deporte Tipo de deporte de la pista
+     * @param stamp $fecha Fecha de la pista
+     * @param int $id Número id de la pista
+     * @return Pistas con el filtro aplicado
      */
     public function datospistafilter( $superficie,$cercamiento, $pared, $deporte, $fecha, $id)
     {
@@ -58,13 +69,20 @@ class ReserveFieldController extends Controller
 
     /**
      *
-     * Devuelve todas las reservas echas en este dia según el filtro
+     * Devuelve todas las reservas hechas en este dia según el filtro
      *
      * Recupera todos los datos de las reservas echas en 1 club
      * y 1 día específico según los filtros que se aplican en la búsqueda y
      * los retorna
      *
      * @author ArnauRovira
+     * @param string $superficie Superfície de la pista
+     * @param string $cercamiento Tipo de suelo de la pista
+     * @param string $pared Cercamiento de la pista
+     * @param string $deporte Tipo de deporte de la pista
+     * @param stamp $fecha Fecha de la pista
+     * @param int $id Número id de la pista
+     * @return Reserva/as realizada
      */
     public function datosreservafilter($superficie,$cercamiento, $pared, $deporte, $fecha, $id)
     {
@@ -73,12 +91,15 @@ class ReserveFieldController extends Controller
 
     /**
      *
-     * Devuelve todas las reservas echas en este dia
+     * Devuelve todas las reservas hechas en este dia
      *
      * Recupera todos los datos de las reservas echas en 1 club
      * y 1 día específico y los retorna
      *
      * @author ArnauRovira
+     * @param int $id Número de la id de la reserva
+     * @param stamp $today Fecha de hoy
+     * @return Reserva realizada en el día especificado
      */
     public function datosreserva($id,$today){
         return Reserva::datosreserva($id,$today);
@@ -92,6 +113,7 @@ class ReserveFieldController extends Controller
      * existentes de la base de datos para los filtros de reserva
      *
      * @author ArnauRovira
+     * @return Todos los deportes existentes
      */
     public function datosdeporte(){
         return Deporte::datosdeporte();
@@ -106,6 +128,14 @@ class ReserveFieldController extends Controller
      * devuelve el container de reservas
      *
      * @author ArnauRovira
+     * @param string $superficie Superfície de la pista
+     * @param string $cercamiento Tipo de suelo de la pista
+     * @param string $pared Cercamiento de la pista
+     * @param string $deporte Tipo de deporte de la pista
+     * @param stamp $today Fecha de la pista
+     * @param int $id Número id de la pista
+     * @param int $id Número id del usuario
+     * @return \Illuminate\View\View Vista con las pistas especificadas
      */
     public function filters($superficie,$cercamiento,  $pared, $deporte, $today, $idclub, $iduser){
         $datospista = $this->datospistafilter($superficie,$cercamiento,  $pared, $deporte, $today, $idclub);
@@ -127,6 +157,9 @@ class ReserveFieldController extends Controller
      * retorna la vista
      *
      * @author ArnauRovira
+     * @param int $idclub Número de id del club
+     * @param int $iduser Número de id del usuario
+     * @return \Illuminate\View\View Vista con los filtros aplicados
      */
     public function timetable($idclub, $iduser){
 
@@ -152,6 +185,10 @@ class ReserveFieldController extends Controller
      * y pistas para retornarlos a la vista
      *
      * @author ArnauRovira
+     * @param int $idclub Número de id del club
+     * @param int $iduser Número de id del usuario
+     * @param stamp $fecha Fecha que específica el usuario
+     * @return \Illuminate\View\View Vista con los datos del establecimiento y de las pistas para las reservas
      */
     public function timetablepart($idclub, $iduser,$today){
 
@@ -170,6 +207,10 @@ class ReserveFieldController extends Controller
      * respecto la hora que se le pasa
      *
      * @author ArnauRovira
+     * @param int $id_pista Número de la id de la pista
+     * @param stamp $fecha_total Fecha total de la reserva
+     * @param stamp $dia Día especificado
+     * @return Objeto de la reserva
      */
     public function timenextbook($id_pista, $fecha_total, $dia){
         return Reserva::timenextbook($id_pista, $fecha_total, $dia);
@@ -183,6 +224,8 @@ class ReserveFieldController extends Controller
      * contiene la id de la pista que se desea
      *
      * @author ArnauRovira
+     * @param int $id Número de la id del estableciminento
+     * @return Objeto con los horarios del establecimiento
      */
     public function datosestablecimientoidpista($id){
         return Establecimiento::datosestablecimientoidpista($id);
@@ -197,6 +240,11 @@ class ReserveFieldController extends Controller
      * dicha pista
      *
      * @author ArnauRovira
+     * @param stamp $fecha Fecha que especifica el usuario
+     * @param stamp $hora Hora que especifica el usuario
+     * @param int $id Número de la id de la pista
+     * @param int $id Número de la id del usuario
+     * @return \Illuminate\View\View Vista con los horarios que el usuario puede reservar
      */
     public function detailtimetable($fecha, $hora, $id_pista, $id_user){
         $datetotal = $fecha." ".$hora;
@@ -218,11 +266,12 @@ class ReserveFieldController extends Controller
 
     /**
      *
-     * Descripción básica (1 linea)
+     * Devuelve los precios de la pista
      *
-     * Descripción detallada
      *
      * @author nickGithub
+     * @param int $id Número de la id de la pista
+     * @return Objeto con los precios de la pista
      */
     public function getpreciopista($idpista){
         return Pista::getpreciopista($idpista);
@@ -237,6 +286,11 @@ class ReserveFieldController extends Controller
      * devuelve una vista mostrando que se ha reservado correctamente
      *
      * @author ArnauRovira
+     * @param stamp $finalhour Fecha inicial que especifica el usuario
+     * @param stamp $initialhour Fecha final que especifica el usuario
+     * @param int $iduser Número de la id del usuario
+     * @param stamp $date Fecha final
+     * @param int $idpista Número de la id de la pista
      */
     public function insertbookbd($finalhour, $initialhour, $iduser, $date, $idpista){
 
